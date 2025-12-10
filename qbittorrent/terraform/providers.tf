4c91a87768ce2d485c5f0f53eae985b4654597da
Paras Sekhon <paras.sekhon@gmail.com>
2025-12-10 21:21:20 +1100

    qbit terraform - working on manual install b4 ansible

---
+++ qbittorrent/terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.2-rc05"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://${var.ip}:8006/api2/json"
+  pm_tls_insecure = true
+
+
+}
+
